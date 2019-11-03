515b194afe3b632cca39dc08a52cc9bdaa49fec8
stepaniuk.artur <211650@edu.p.lodz.pl>
2019-11-03 10:57:11 +0100

    route for creating lists added

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,4 +9,8 @@ public func routes(_ router: Router) throws {
     router.get("all", use: tableController.all)
     router.post("create/table", use: tableController.createNewTable)
     router.put("update/table/title", use: tableController.updateTableTitle)
+    
+    let listController = ListController()
+    router.post(CreateListRequest.self, at: "tables", Int.parameter, "lists", "create",
+                use: listController.create)
 }
