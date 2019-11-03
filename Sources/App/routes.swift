785ca076fa1b75aebef4f84330707c7e7594da9f
stepaniuk.artur <211650@edu.p.lodz.pl>
2019-11-03 11:38:37 +0100

    routes for lists changed

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,6 +11,8 @@ public func routes(_ router: Router) throws {
     router.put("update/table/title", use: tableController.updateTableTitle)
     
     let listController = ListController()
-    router.post(CreateListRequest.self, at: "tables", Int.parameter, "lists", "create",
-                use: listController.create)
+    router.post(CreateListRequest.self, at: "lists", "create", use: listController.create)
+    router.get("lists", use: listController.getAll)
+    router.get("lists", List.parameter, use: listController.getById)
 }
+    
